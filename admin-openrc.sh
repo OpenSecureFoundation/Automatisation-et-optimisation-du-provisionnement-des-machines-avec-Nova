6fc79c17d7a8460f8c9ad13eb56822e66bc922e3
fab1314 <fabiolamemezague@gmail.com>
2026-03-03 20:37:49 +0100

    deploiemnet initial

---
+++ admin-openrc.sh
@@ -0,0 +1,7 @@
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=keystone  # Remplacez par le mot de passe choisi à l'install de Keystone
+export OS_AUTH_URL=http://192.168.212.136:5000/v3
+export OS_IDENTITY_API_VERSION=3
